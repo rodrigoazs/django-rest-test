47efd7a410d5f71714f334fd0369450ce793a466
Rodrigo Azevedo <ext.rodrigo.azevedo@farfetch.com>
2022-01-11 23:02:39 +0000

    update django on heroku

--- bank/settings.py
+++ bank/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+import django_on_heroku
 
 from pathlib import Path
 
@@ -130,4 +130,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # django_heroku
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
