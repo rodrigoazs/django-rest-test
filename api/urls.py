f81d5cb1995da2e1454c1abc41024b4072f75af3
Rodrigo Azevedo <ext.rodrigo.azevedo@farfetch.com>
2022-01-13 20:45:07 +0000

    fix withdrawal url

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("create_account/", views.AccountCreateView.as_view(), name="account_create"),
     path("balance/", views.AccountBalanceView.as_view(), name="balance"),
     path("deposit/<str:username>/", views.DepositView.as_view(), name="deposit"),
-    path("withdrawal/<int:pk>/", views.WithdrawalView.as_view(), name="withdrawal"),
+    path("withdrawal/", views.WithdrawalView.as_view(), name="withdrawal"),
 ]
