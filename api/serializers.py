7107271f33d9971c384af621d66ecf62c1078e21
Rodrigo Azevedo <ext.rodrigo.azevedo@farfetch.com>
2022-01-13 20:48:28 +0000

    comment serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.Serializer):
+    """Serializer for user model"""
+
     username = serializers.CharField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
@@ -10,8 +12,12 @@ class UserSerializer(serializers.Serializer):
 
 
 class AccountBalanceSerializer(serializers.Serializer):
+    """Serializer for account balance"""
+
     balance = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
 
 class AmountSerializer(serializers.Serializer):
+    """Serializer for deposit/withdrawal request"""
+
     amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.0)
